c548402f487ad4c2a9239bda35b33837453f4917
Sebastien Binet <binet@cern.ch>
2012-12-12 09:35:19 +0100

    typos

--- inc/pkg-aa/h1d.hh
+++ inc/pkg-aa/h1d.hh
@@ -1,9 +1,9 @@
-#ifndef ROOTCOMPS_TESTH1D_HH
-#define ROOTCOMPS_TESTH1D_HH 1
+#ifndef PKG_AA_H1D_HH
+#define PKG_AA_H1D_HH 1
 
 namespace pkg_aa {
   void test_th1d();
 }
 
-#endif /*ROOTCOMPS_TESTH1D_HH */
+#endif /*PKG_AA_H1D_HH */
 
