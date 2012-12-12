e3a1f91a627d116bdee9a565b94197be635223f7
Sebastien Binet <binet@cern.ch>
2012-12-12 10:55:40 +0100

    fixup namespace

--- src/pkg-aa.cxx
+++ src/pkg-aa.cxx
@@ -3,11 +3,13 @@
 
 #include "pkg-aa/h1d.hh"
 
-using namespace pkg_aa;
+namespace pkg_aa {
 
 void test_h1d()
 {
   TH1D *h = new TH1D("h1-aa", "h1-aa", 100, 0., 100.);
   std::cout << "h1-aa: " << h->GetEntries() << "\n";
   delete h; h = NULL;
+}
+
 }
